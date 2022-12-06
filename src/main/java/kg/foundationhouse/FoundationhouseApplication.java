12456f57cb94aadcc2aa1bb7b25ef43a602583ad
Zamira <keldibaevaz@gmail.com>
2022-12-06 17:46:04 +0300

    Initial commit

---
+++ src/main/java/kg/foundationhouse/FoundationhouseApplication.java
@@ -0,0 +1,13 @@
+package kg.foundationhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoundationhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoundationhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/foundationhouse/FoundationhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.foundationhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoundationhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
